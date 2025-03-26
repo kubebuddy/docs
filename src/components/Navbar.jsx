1d823ee6e5504900d30e41f4b8ff4f505bf49cdc
Pulkit-thinknyx <pulkit.khandelwal@thinknyx.com>
2025-03-26 15:02:18 +0530

    Modified navbar to display on mobile divices as well

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,51 +1,86 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+
 const Navbar = () => {
+  const location = useLocation();
+  const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+  useEffect(() => {
+    const handleResize = () => setIsMobile(window.innerWidth < 768);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
-    <nav className="navbar navbar-expand-lg navbar-light border-bottom py-2 w-auto position-sticky fixed-top" style={{backgroundColor : "rgb(248, 249, 250)"}}>
+    <nav className="navbar navbar-expand-lg navbar-light border-bottom py-2 w-auto position-sticky fixed-top" style={{ backgroundColor: "rgb(248, 249, 250)" }}>
       <div className="container-fluid px-4">
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-      </button>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
         {/* Logo and brand */}
-        <div className="navbar-brand d-flex align-items-center" href="/">
-        <img src={"/KubeBuddy-no-title.png"} alt="KubeBuddy Logo" className="img-fluid mx-2" style={{ maxWidth: "40px" }} />
-            <NavLink className="navbar-brand fw-semibold" to="/"><span className="text-primary fw-bold league-spartan">KUBE</span><span className="fw-bold league-spartan">BUDDY</span></NavLink>
+        <div className="navbar-brand d-flex align-items-center">
+          <img src={"/KubeBuddy-no-title.png"} alt="KubeBuddy Logo" className="img-fluid mx-2" style={{ maxWidth: "40px" }} />
+          <NavLink className="navbar-brand fw-semibold" to="/"><span className="text-primary fw-bold league-spartan">KUBE</span><span className="fw-bold league-spartan">BUDDY</span></NavLink>
         </div>
-        
-        {/* Main navigation links */}
+
+        {/* Navigation Links for Docs page (Visible only in /docs and Mobile View) */}
+        { (isMobile && location.pathname.includes("/docs")) ? (
+          <div className="collapse navbar-collapse m-2" id="navbarNav">
+            <ul className="nav flex-column">
+              <li className="nav-item">
+                <NavLink className={({ isActive }) => `nav-link text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs/introduction">Introduction</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={({ isActive }) => `nav-link text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs/installation">Installation</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className={({ isActive }) => `nav-link text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs/platform">Platform</NavLink>
+              </li>
+
+              {/* Guide Section */}
+              <li className="nav-item">
+                <h6 className="fw-bold text-dark m-2">Guide</h6>
+                <ul className="nav flex-column ps-3">
+                  <li className="nav-item">
+                    <NavLink className={({ isActive }) => `nav-link text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs/guide/getting-started">Getting Started</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className={({ isActive }) => `nav-link text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs/guide/configuration">Configuration</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className={({ isActive }) => `nav-link text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs/guide/buddyai">Buddy AI</NavLink>
+                  </li>
+                  <li className="nav-item">
+                    <NavLink className={({ isActive }) => `nav-link text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs/guide/terminal">Terminal</NavLink>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        ) :
         <div className="collapse navbar-collapse m-2" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item mx-1">
-                <NavLink 
-                    className={({ isActive }) => `nav-link px-3 py-2 text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} 
-                    to="/" 
-                    end
-                >
-                    Home
-                </NavLink>
+              <NavLink className={({ isActive }) => `nav-link px-3 py-2 text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/" end>
+                Home
+              </NavLink>
             </li>
             <li className="nav-item mx-1">
-            <NavLink 
-                className={({ isActive }) => `nav-link px-3 py-2 text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} 
-                to="/docs"
-              >
+              <NavLink className={({ isActive }) => `nav-link px-3 py-2 text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/docs">
                 Docs
               </NavLink>
             </li>
             <li>
-              <NavLink className={({isActive}) => `nav-link px-3 py-2 text-dark fw-medium rounded ${isActive ? "active bg-primary text-white": "hover-bg-light"}`} 
-              to={"/faqs"}
-              >
+              <NavLink className={({ isActive }) => `nav-link px-3 py-2 text-dark fw-medium rounded ${isActive ? "active bg-primary text-white" : "hover-bg-light"}`} to="/faqs">
                 FAQs
               </NavLink>
             </li>
           </ul>
-        </div>
-        
+        </div>}
+
         {/* Right-side items */}
         <div className="d-flex align-items-center">
-          {/* External links */}
-          
           <a href="https://github.com/kubebuddy/docs" className="text-decoration-none text-dark me-3 d-flex align-items-center" target="_blank" rel="noopener noreferrer">
             GitHub
             <svg className="ms-1" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -54,7 +89,6 @@ const Navbar = () => {
               <path d="M20 4L10 14" stroke="currentColor" strokeWidth="2" />
             </svg>
           </a>
-          
         </div>
       </div>
     </nav>
